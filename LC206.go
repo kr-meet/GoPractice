329dd8bdd284eb282560f8b3a6a1ea939bf04f39
kr-meet <meet.k@knackroot.com>
2023-07-22 21:05:46 +0530

    Leetcode206

---
+++ LC206.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    
+    var previous *ListNode
+    
+    for head != nil {
+        next := head.Next
+        head.Next = previous
+        previous = head
+        head = next
+    }
+
+    return previous
+}
